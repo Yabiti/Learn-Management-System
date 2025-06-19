350a75c2582e006384a8408028c74231e65676c7
Yabiti <yabetseyohannes@gmail.com>
2025-06-19 13:53:10 +0300

    .

--- lms/src/app/page.js
+++ lms/src/app/page.js
@@ -1,8 +1,16 @@
 export default function Home() {
     return(
+        <div>
         <div className="Home-page">
             <h1>Transform Your Future with EduCore — Your Trusted <br />Platform for Lifelong Learning and Professional Growth</h1>
             <p className="para">Learn smarter, grow faster. EduCore is your trusted partner for lifelong learning and professional development — all in one easy-to-use platform.</p>
+        </div>
+
+        <form>
+            <input type="text" placeholder="search for courses" />
+            <button type="submit">Search</button>
+
+        </form>
         </div>
     );
 }
