b898b4d9b5ca978a878330f788e9b9d414d9f2cc
Yabiti <yabetseyohannes@gmail.com>
2025-06-19 13:41:02 +0300

    .

--- lms/src/app/Navbar.jsx
+++ lms/src/app/Navbar.jsx
@@ -12,5 +12,5 @@ export default function Navbar(){
                 <Link href="/">Student Enrollment</Link>
             </div>
         </nav>
-    )
+    );
 }
--- lms/src/app/page.js
+++ lms/src/app/page.js
@@ -1,9 +1,8 @@
-import Link from "next/link";
-
-export default function Home(){
+export default function Home() {
     return(
-        <div className="home-page">
-            <h1>Transform Your Future with EduCore — Your Trusted Platform for Lifelong Learning and Professional Growth</h1>
+        <div className="Home-page">
+            <h1>Transform Your Future with EduCore — Your Trusted <br />Platform for Lifelong Learning and Professional Growth</h1>
+            <p className="para">Learn smarter, grow faster. EduCore is your trusted partner for lifelong learning and professional development — all in one easy-to-use platform.</p>
         </div>
     );
 }
