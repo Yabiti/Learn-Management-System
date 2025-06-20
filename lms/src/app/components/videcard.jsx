19416bac835be6618a76ec031411aa21302e33d1
Yabiti <yabetseyohannes@gmail.com>
2025-06-20 10:35:36 +0300

    .

--- lms/src/app/components/videcard.jsx
+++ lms/src/app/components/videcard.jsx
@@ -1,7 +1,7 @@
 export const video = [
     {
         id: 1,
-        video: "/video/Dom",
+        url: 'https://youtu.be/ajdRvxDWH4w?si=2WGt441vBi51UVgD/embed/ajdRvxDWH4w',
         title: "Javscript Basics",
         name: 'greatstock',
         price: '39.99$$'
--- lms/src/app/video/id/page.js
+++ lms/src/app/video/id/page.js
@@ -1,10 +1,1 @@
-
-export default function VideoPage({params}){
-    const {id} = params;
-
-    return(
-        <div>
-            <h1>Video {id}</h1>
-        </div>
-    );
-}
+import {video} from "./components/videocard.jsx";
