dd1dc6d82802633ed7e8c3d980fe64e49bd25e1c
Yabiti <yabetseyohannes@gmail.com>
2025-06-20 10:41:42 +0300

    .

--- lms/src/app/components/videcard.jsx
+++ lms/src/app/components/videcard.jsx
@@ -1,7 +1,7 @@
 export const video = [
     {
         id: 1,
-        url: 'https://youtu.be/ajdRvxDWH4w?si=2WGt441vBi51UVgD/embed/ajdRvxDWH4w',
+        url: 'https://www.youtube.com/embed/ajdRvxDWH4w',
         title: "Javscript Basics",
         name: 'greatstock',
         price: '39.99$$'
--- lms/src/app/video/id/page.js
+++ lms/src/app/video/id/page.js
@@ -1,1 +1,19 @@
-import {video} from "./components/videocard.jsx";
+import {videos} from "./components/videocard.jsx";
+
+export default function Home(){
+    return(
+        <div className="video-editor">
+            {videos.map((video) =>
+            <div hey={video.id}>
+                <iframe
+                    src ='{video.url'
+                    title = '{video.title}'
+                    ></iframe>
+                    <h3>{video.title}</h3>
+                    <p>{video.price}</p>
+            </div>
+
+            )}
+        </div>
+    );
+}
