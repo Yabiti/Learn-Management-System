c665042f1011c9159de5ea9acee24779f4d25827
Yabiti <yabetseyohannes@gmail.com>
2025-06-20 13:53:08 +0300

    .

--- lms/src/app/components/Footer.jsx
+++ lms/src/app/components/Footer.jsx
@@ -3,11 +3,13 @@ import "../globals.css";
 
 export default function Footer(){
     return(
-        <footer className="navbar">
-            <div className="logo">
-                <h1>EduCore</h1>
-                <p>Learn anytime, anywhere. EduCore helps you grow your <br/>skills and reach your goals — one lesson at a time.</p>
+        <div>
+            <footer className="navbar">
+                <div className="logo">
+                    <h1>EduCore</h1>
+                <   p className="pa">Learn anytime, anywhere. EduCore helps you grow your <br/>skills and reach your goals — one lesson at a time.</p>
+                </div>
+            </footer>
             </div>
-        </footer>
     );
 }
