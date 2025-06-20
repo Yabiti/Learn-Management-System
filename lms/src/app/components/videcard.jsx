103a5cddcbf3e3485f65001c2286f29d4d63e547
Yabiti <yabetseyohannes@gmail.com>
2025-06-20 09:57:06 +0300

    .

--- lms/src/app/components/videcard.jsx
+++ lms/src/app/components/videcard.jsx
@@ -1,1 +1,9 @@
-export const video =
+export const video = [
+    {
+        id: 1,
+        video: "/video/Dom",
+        title: "Javscript Basics",
+        name: 'greatstock',
+        price: '39.99$$'
+    }
+]
