d7edd29a1d52a652e563575e3528989135e6303c
Yabiti <yabetseyohannes@gmail.com>
2025-06-24 09:34:08 +0300

    .

--- lms/src/app/video/[id]/page.js
+++ lms/src/app/video/[id]/page.js
@@ -20,26 +20,7 @@ export default function CoursePage() {
         </span>
         ))}
       </p>
-            <div className='video-editor'>
-                {visiblevideos.map(video => (
-                    <Link key={video.id} href={`/video/${video.id}`} className='video-card'>
-                        <img
-                        src={video.thumbnail}
-                        title={video.title}
-                        name={video.name}
-                        rating={video.rating}
-                        ></img>
-                        <div className='video-details'>
-                        <h3 className='title'>{video.title}</h3>
-                        <p>{video.name}</p>
-                        <div className="rating">
-                            {'★'.repeat(video.rating) + '☆'.repeat(5 - video.rating)}
-                        </div>
-                        <p>{video.price}</p>
-                    </div>
-                    </Link>
-                ))}
-            </div>
+
     </div>
   );
 }
