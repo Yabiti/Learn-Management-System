8d08552e433c9c1aa9d9c3a63d6794f997745eaf
Yabiti <yabetseyohannes@gmail.com>
2025-06-25 10:20:24 +0300

    .

--- lms/src/app/components/VideoCard.jsx
+++ lms/src/app/components/VideoCard.jsx
@@ -18,8 +18,8 @@ export const videos = [
         name: 'greatstock',
         rating: 4,
         price: '39.99$$',
-        url: "https://www.youtube.com/embed/ajdRvxDWH4w" ,
-        description: "Learn the fundamentals of HTML and CSS to build beautiful websites.",
+        url: "https://www.youtube.com/embed/ajdRvxDWH4w",
+        description: "This course provides a comprehensive introduction to the foundational building blocks of the web:\n HTML and CSS. You'll learn how to structure web pages using semantic HTML elements and bring\n them to life with beautifully styled layouts using CSS. Starting from basic tags and styles, you'll gradually progress\n to advanced topics.",
         thumbnail: 'images/js.png',
 
     },
