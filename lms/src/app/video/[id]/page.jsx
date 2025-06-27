dd9bd3247d0fcea15a3f7cafcafe93fd51add6f7
Yabiti <yabetseyohannes@gmail.com>
2025-06-27 11:08:58 +0300

    .

--- lms/src/app/video/[id]/page.jsx
+++ lms/src/app/video/[id]/page.jsx
@@ -34,7 +34,7 @@ export default function CoursePage() {
             className="thumbnail"
           />
         ) : (
-          <iframe
+          <iframe className="i"
             src={video.url}
             title={video.title}
             frameBorder="0"
